696e58f08087fcc699f01d9360b510707e464c53
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-21 18:25:01 +0300

    Add problem link to Climbing Stairs solution
    Added problem link for Climbing Stairs problem.

---
+++ leetcode/dynamic_programming/70_Climbing_Stairs.cpp
@@ -0,0 +1,21 @@
+//problem link : https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution {
+public:
+    static const int N = 45+1;//46
+    int dp[N];
+    Solution(){
+        memset(dp,-1,sizeof(dp));
+    }
+    int climbStairs(int n) {
+        if(~dp[n]) return dp[n];
+        if(n<1) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;//fix : forgetting to set the base case when n == 2
+        int ch1=climbStairs(n-1);
+        int ch2=climbStairs(n-2);
+    
+        return dp[n] =ch1+ch2;
+
+    }
+};
