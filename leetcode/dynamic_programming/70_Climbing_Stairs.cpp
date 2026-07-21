5529c682c7ad8bbb0a870d90613e30c56053e4bd
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-21 18:30:10 +0300

    Refactor climbStairs base case for optimization
    Optimize base case handling for climbStairs function.

--- leetcode/dynamic_programming/70_Climbing_Stairs.cpp
+++ leetcode/dynamic_programming/70_Climbing_Stairs.cpp
@@ -9,9 +9,15 @@ public:
     }
     int climbStairs(int n) {
         if(~dp[n]) return dp[n];
+        
+        /*
         if(n<1) return 0;
         if(n==1) return 1;
         if(n==2) return 2;//fix : forgetting to set the base case when n == 2
+        */
+        ////instead : 
+        if(n<=2) return n;//small optimization and actually it can not reach negatives with this so it is ok
+        
         int ch1=climbStairs(n-1);
         int ch2=climbStairs(n-2);
     
