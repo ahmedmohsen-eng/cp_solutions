1ec2179c6bea0a885423872395c81a90ac675e35
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-21 18:04:43 +0300

    Implement N-th Tribonacci Number solution

---
+++ leetcode/dynamic_programming/N-th_Tribonacci_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    static const int N=38;
+    int dp[N];
+    Solution() {
+        memset(dp, -1, sizeof(dp));
+    }
+    int tribonacci(int n) {
+
+        if(n==0) return 0;
+        if(n==1||n==2) return 1;
+
+        if(~dp[n])return dp[n];
+
+        return dp[n] = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3); //don't forget to store not only returning
+
+    }
+};
